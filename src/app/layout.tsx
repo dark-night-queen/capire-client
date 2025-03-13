ef3ced7078add930c3e145af14850dab810be9ce
Shreya <agrawal.shreya@icloud.com>
2025-03-13 11:08:27 +0530

    added tailwind.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
+import { ThemeProvider } from "@/lib/providers/theme-provider";
 
+// Load the Inter font
 const inter = Inter({ subsets: ["latin"] });
 
+// Metadata for the app
 export const metadata: Metadata = {
   title: "Capire",
   description: "Reading and Understanding Stories",
@@ -13,17 +16,32 @@ export const metadata: Metadata = {
       url: "/favicon.ico",
       href: "/favicon.ico",
     },
+    {
+      media: "(prefers-color-scheme: dark)",
+      url: "/dark-favicon.ico",
+      href: "/dark-favicon.ico",
+    },
   ],
 };
 
+// Root layout for the app
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
