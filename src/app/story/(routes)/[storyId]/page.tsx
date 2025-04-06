62396e5cfdc9e4b76a462cf5bf7d89e4686906ef
Shreya <agrawal.shreya@icloud.com>
2025-04-06 20:25:57 +0530

    story page to include gradient on overview and description

--- src/app/story/(routes)/[storyId]/page.tsx
+++ src/app/story/(routes)/[storyId]/page.tsx
@@ -1,12 +1,15 @@
 import { Grid } from "@/lib/components/grid";
 import { Overview, Description, Related, Comments } from "../../_components";
 import { dummyStory } from "@/lib/dummy/story";
+import { Box } from "@/lib/components/box";
 
 export default function Story() {
   return (
     <Grid gap={6}>
-      <Overview />
-      <Description description={dummyStory.description} />
+      <Box className="bg-gradient-to-b from-teal-500 to-transparent">
+        <Overview {...dummyStory} />
+        <Description description={dummyStory.description} />
+      </Box>
       <Related />
       <Comments />
     </Grid>
