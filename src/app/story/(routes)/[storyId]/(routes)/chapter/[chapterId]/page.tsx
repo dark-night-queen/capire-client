b2c9fd1aeac3dc484d9121c6b52f71b4a1d5a5b8
Shreya <agrawal.shreya@icloud.com>
2025-04-08 01:19:28 +0530

    ran prettier

--- src/app/story/(routes)/[storyId]/(routes)/chapter/[chapterId]/page.tsx
+++ src/app/story/(routes)/[storyId]/(routes)/chapter/[chapterId]/page.tsx
@@ -10,13 +10,13 @@ export default function Chapter() {
   const chapterDetails = {
     title: "Chapter I",
     content: `The Duke of Lancaster who sought independence from the empire! \n
-    Toppled by the Emperor’s suppression, the young and beautiful Duchess Ethel Lancaster falls as spoils of war. 
+    Toppled by the Emperor’s suppression, the young and beautiful Duchess Ethel Lancaster falls as spoils of war.\n \n
     Standing before the nobles who, not long ago, bowed their heads to her like a slave! Regardless of who takes possession, 
     it is certain that she will become a target for exploitation…. The Emperor seeks consent from a man of humble origin. 
     From the mercenary Count, Rasto Christceros. “Rasto, do you not need a woman?” “Well, I do lack a maid at home.” 
-    Rasto’s response captivates the Emperor, who wanted to set an example for the nobles. ***
+    Rasto’s response captivates the Emperor, who wanted to set an example for the nobles. \n***
     Ethel, now a ‘possession’ of Rasto, contemplates ending her life before suffering disgrace, but Rasto orders her to work
-    as a maid. However, in the eyes of the once perfect Duchess, Count Christceros seems terribly lacking. From the dreary interior of the mansion, cheap decorations, indifferent servants, to the groveling maids acting like those in power…. ‘Is revealing this going too far, or is it a crucial point?’ The highly experienced novice maid, Ethel, begins to gradually change County Christceros. Including the master of the house, Rasto Christceros.`,
+    as a maid. \n However, in the eyes of the once perfect Duchess, Count Christceros seems terribly lacking. From the dreary interior of the mansion, cheap decorations, indifferent servants, to the groveling maids acting like those in power…. ‘Is revealing this going too far, or is it a crucial point?’ The highly experienced novice maid, Ethel, begins to gradually change County Christceros. Including the master of the house, Rasto Christceros.`,
   };
 
   const { title, content } = chapterDetails;
@@ -40,7 +40,7 @@ export default function Chapter() {
           />
         </CardHeader>
 
-        <CardContent>
+        <CardContent className="whitespace-pre-line">
           <span className={`${rye.className} capitalize text-4xl`}>
             {firstCharacter}
           </span>
--- src/app/story/_components/Related.tsx
+++ src/app/story/_components/Related.tsx
@@ -14,5 +14,5 @@ export const Related = () => {
         <CardDescription>...</CardDescription>
       </CardHeader>
     </Card>
-  )
-}
+  );
+};
